a697c8734741312f3c672fbe8e5edea981cd5bc8
Chad O'Brien <obrien.music@gmail.com>
2021-09-19 23:18:01 -0400

    load finished challenge for debugging

--- js/kanban.js
+++ js/kanban.js
@@ -18,11 +18,40 @@ add_btn.addEventListener('click', () => {
 const create_item = () => {
   var item = document.createElement('div');
   item.classList.add('item');
-  item.id = `item-${order}`;
-  item.addEventListener("dragstart", (event) => {
-    item.dataTransfer.setData("text", event.target.id)
+  item.id = 'item-' + order;
+  item.draggable = true;
+  item.addEventListener('dragstart', (event) => {
+    event.dataTransfer.setData("text", event.target.id)
   });
+  item.addEventListener('dragend', (event) => {
+    event.dataTransfer.clearData();
+  });
+  var input = document.createElement('input');
+  item.appendChild(input);
+  var save_btn = document.createElement('button');
+  save_btn.innerHTML = 'Save';
+  save_btn.addEventListener('click', () => {
+    error.innerHTML = '';
+    if (input.value !== '') {
+      order++;
+      item.innerHTML = input.value;
+      adding = false;
+    } else {
+      error.innerHTML = message;
+    }
+  })
+  item.appendChild(save_btn);
+
+  return item;
 };
 
 document.querySelectorAll('.drop').forEach(element => {
+  element.addEventListener('drop', (event) => {
+    event.preventDefault();
+    const id = event.dataTransfer.getData('text')
+    document.getElementById('id').append(event.target);
+  })
+  element.addEventListener('dragover', (event) => {
+    event.preventDefault;
+  })
 });
